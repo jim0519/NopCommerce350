c23eef3789e8fec147c094faa78103c771a9cda9
Jim <gdutjim@gmail.com>
2017-10-20 11:28:03 +1100

    Startup Commit

---
+++ Presentation/Nop.Web/Controllers/KeepAliveController.cs
@@ -0,0 +1,27 @@
+﻿using System.Web.Mvc;
+
+namespace Nop.Web.Controllers
+{
+    //do not inherit it from BasePublicController. otherwise a lot of extra acion filters will be called
+    //they can create guest account(s), etc
+    public partial class KeepAliveController : Controller
+    {
+        public ActionResult Index()
+        {
+            return Content("I am alive!");
+        }
+
+        //public ActionResult TestWebBrowser()
+        //{
+        //    WebBrowser wb = new WebBrowser();
+        //    wb.ScriptErrorsSuppressed = true;
+
+        //    wb.Navigate("http://www.gumtree.com.au");
+        //    while (wb.ReadyState != WebBrowserReadyState.Complete)
+        //    {
+        //        Application.DoEvents();
+        //    }
+        //    return Content(wb.DocumentText);
+        //}
+    }
+}
