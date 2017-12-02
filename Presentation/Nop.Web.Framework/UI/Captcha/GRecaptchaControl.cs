eab4458082da2ad4000418ba864b912978835e90
Jim <gdutjim@gmail.com>
2017-12-02 16:23:49 +1100

    Captcha 2.0 change

---
+++ Presentation/Nop.Web.Framework/UI/Captcha/GRecaptchaControl.cs
@@ -0,0 +1,74 @@
+﻿using System.Linq;
+using System.Web.Mvc;
+using System.Web.UI;
+using Nop.Core;
+using Nop.Core.Infrastructure;
+
+namespace Nop.Web.Framework.UI.Captcha
+{
+    public class GRecaptchaControl
+    {
+        private const string RECAPTCHA_API_URL_HTTP_VERSION1 = "http://www.google.com/recaptcha/api/challenge?k={0}";
+        private const string RECAPTCHA_API_URL_HTTPS_VERSION1 = "https://www.google.com/recaptcha/api/challenge?k={0}";
+        private const string RECAPTCHA_API_URL_VERSION2 = "https://www.google.com/recaptcha/api.js?onload=onloadCallback&render=explicit";
+
+        public string Id { get; set; }
+        public string Theme { get; set; }
+        public string PublicKey { get; set; }
+        public string Language { get; set; }
+
+        //private readonly ReCaptchaVersion _version;
+
+        public GRecaptchaControl()
+        {
+            
+        }
+
+        public void RenderControl(HtmlTextWriter writer)
+        {
+            SetTheme();
+
+
+            var scriptCallbackTag = new TagBuilder("script");
+            scriptCallbackTag.Attributes.Add("type", "text/javascript");
+            scriptCallbackTag.InnerHtml = string.Format("var onloadCallback = function() {{grecaptcha.render('{0}', {{'sitekey' : '{1}', 'theme' : '{2}' }});}};", Id, PublicKey, Theme);
+            writer.Write(scriptCallbackTag.ToString(TagRenderMode.Normal));
+
+            var captchaTag = new TagBuilder("div");
+            captchaTag.Attributes.Add("id", Id);
+            writer.Write(captchaTag.ToString(TagRenderMode.Normal));
+
+            var scriptLoadApiTag = new TagBuilder("script");
+            scriptLoadApiTag.Attributes.Add("src", RECAPTCHA_API_URL_VERSION2 + (string.IsNullOrEmpty(Language) ? "" : string.Format("&hl={0}", Language)));
+            scriptLoadApiTag.Attributes.Add("async", null);
+            scriptLoadApiTag.Attributes.Add("defer", null);
+            writer.Write(scriptLoadApiTag.ToString(TagRenderMode.Normal));
+            
+        }
+
+        private void SetTheme()
+        {
+            var themes = new[] {"white", "blackglass", "red", "clean", "light", "dark"};
+
+
+            switch (Theme.ToLower())
+            {
+                case "clean":
+                case "red":
+                case "white":
+                    Theme = "light";
+                    break;
+                case "blackglass":
+                    Theme = "dark";
+                    break;
+                default:
+                    if (!themes.Contains(Theme.ToLower()))
+                    {
+                        Theme = "light";
+                    }
+                    break;
+            }
+            
+        }
+    }
+}
--- Presentation/Nop.Web.Framework/UI/Captcha/HtmlExtensions.cs
+++ Presentation/Nop.Web.Framework/UI/Captcha/HtmlExtensions.cs
@@ -7,17 +7,38 @@ namespace Nop.Web.Framework.UI.Captcha
 {
     public static class HtmlExtensions
     {
+        //public static string GenerateCaptcha(this HtmlHelper helper)
+        //{
+        //    var captchaSettings = EngineContext.Current.Resolve<CaptchaSettings>();
+
+        //    var theme = !string.IsNullOrEmpty(captchaSettings.ReCaptchaTheme) ? captchaSettings.ReCaptchaTheme : "white";
+        //    var captchaControl = new Recaptcha.RecaptchaControl
+        //    {
+        //        ID = "recaptcha",
+        //        Theme = theme,
+        //        PublicKey = captchaSettings.ReCaptchaPublicKey,
+        //        PrivateKey = captchaSettings.ReCaptchaPrivateKey
+        //    };
+
+        //    var htmlWriter = new HtmlTextWriter(new StringWriter());
+
+        //    captchaControl.RenderControl(htmlWriter);
+
+        //    return htmlWriter.InnerWriter.ToString();
+        //}
+
         public static string GenerateCaptcha(this HtmlHelper helper)
         {
             var captchaSettings = EngineContext.Current.Resolve<CaptchaSettings>();
 
             var theme = !string.IsNullOrEmpty(captchaSettings.ReCaptchaTheme) ? captchaSettings.ReCaptchaTheme : "white";
-            var captchaControl = new Recaptcha.RecaptchaControl
+            var captchaControl = new GRecaptchaControl
             {
-                ID = "recaptcha",
+                Id = "recaptcha",
                 Theme = theme,
-                PublicKey = captchaSettings.ReCaptchaPublicKey,
-                PrivateKey = captchaSettings.ReCaptchaPrivateKey
+                PublicKey = captchaSettings.ReCaptchaPublicKey
+                //PrivateKey = captchaSettings.ReCaptchaPrivateKey
+
             };
 
             var htmlWriter = new HtmlTextWriter(new StringWriter());
